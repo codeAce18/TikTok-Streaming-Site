79cc6cb8cf40b6d79de93c088fa9004d38282c3a
TechMogul <joeltaiwo20018@gmail.com>
2025-08-23 08:35:22 +0100

    check

--- script.js
+++ script.js
@@ -332,16 +332,16 @@ document.addEventListener("click", (e) => {
     const y = e.clientY - rect.top - size / 2
 
     ripple.style.cssText = `
-            position: absolute;
-            width: ${size}px;
-            height: ${size}px;
-            left: ${x}px;
-            top: ${y}px;
-            background: rgba(255, 255, 255, 0.3);
-            border-radius: 50%;
-            transform: scale(0);
-            animation: ripple 0.6s ease-out;
-            pointer-events: none;
+          position: absolute;
+          width: ${size}px;
+          height: ${size}px;
+          left: ${x}px;
+          top: ${y}px;
+          background: rgba(255, 255, 255, 0.3);
+          border-radius: 50%;
+          transform: scale(0);
+          animation: ripple 0.6s ease-out;
+          pointer-events: none;
         `
 
     button.style.position = "relative"
